5e16c1e7cb2856f4fbfeb16b424fa9d3b1daca4e
Cenk-Ertegi <c.ertegi@gmail.com>
2024-07-02 10:42:40 +0200

    Ach mist man

---
+++ Coding-Challenges/New Group/Woche2/Tag 5.swift
@@ -0,0 +1,9 @@
+//
+//  Tag 5.swift
+//  Coding-Challenges
+//
+//  Created by Cenk Ertegi on 02.07.24.
+//
+
+import Foundation
+print("Hallo")
---
+++ Coding-Challenges/New Group/Woche2/Tag 5/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Coding-Challenges
+//
+//  Created by Cenk Ertegi on 02.07.24.
+//
+
+import Foundation
+
